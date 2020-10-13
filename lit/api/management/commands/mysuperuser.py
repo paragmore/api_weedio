bf6d11d8624729842f0e1f5e31fe43e22eb31547
Parag More <34959548+paragmore@users.noreply.github.com>
2020-10-13 12:19:23 +0530

    hidden credentials do not pull locally

--- lit/api/management/commands/mysuperuser.py
+++ lit/api/management/commands/mysuperuser.py
@@ -6,5 +6,5 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         if not User.objects.filter(username='CurateUp').exists():
             User.objects.create_superuser('CurateUp',
-                                          'contact.curateup@gmail.com',
-                                          'QRate@123')
+                                          'mail',
+                                          'pass')
